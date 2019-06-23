c0d300246c8844ac415725aca6aa31c392305cbe
rohanmittal01 <rohanmittal01@gmail.com>
2019-06-23 12:07:02 +0530

    first commit

---
+++ Desktop/UDEMY XCODE COURSE /i am rich/i am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Vandana Mittal on 10/10/18.
+//  Copyright © 2018 Rohan Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
