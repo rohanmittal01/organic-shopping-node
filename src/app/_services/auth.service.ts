a74ab0dc2ed3333eb9aaa48d43ec30976f30dad6
rohanmittal01 <rohanmittal01@gmail.com>
2020-07-21 21:16:43 +0530

    login service functional and page

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { AlertifyService } from './alertify.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseUrl = "http://localhost:3000/users/"
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+
+  constructor(private http: HttpClient, private route: Router, private alertify: AlertifyService) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        // console.log(user);
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.route.navigate(['/']);
+        }
+      })
+    );
+  }
+
+  loggedIn(){
+    let token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+
+  logout(){
+    if(this.loggedIn()){
+      localStorage.removeItem('token');
+      console.log('logged out!');
+      this.alertify.warning('Logged Out!');
+      this.route.navigate(['/login']);
+    }
+  }
+  register(model: any){
+    // console.log(model);
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  // modify(model: any){
+  //   return this.http.post(this.dataUrl + 'modify', model);
+  // }
+
+  changePassword(model: any){
+    return this.http.post(this.baseUrl + 'newPassword', model);
+  }
+
+
+
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'login',
@@ -10,9 +12,22 @@ export class LoginComponent implements OnInit {
   model: any = {};
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
 
-  constructor() { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.model);
+    this.authService.login(this.model).subscribe((next) => {
+      console.log(next)
+      this.alertify.success('Logged in successfully');
+      const token = localStorage.getItem('token');
+      console.log(token)
+      
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
