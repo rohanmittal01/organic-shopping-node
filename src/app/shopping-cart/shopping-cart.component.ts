ba053c93fac0aba16d84c4832bec102f20c53d49
rohanmittal01 <rohanmittal01@gmail.com>
2020-07-30 01:14:02 +0530

    available & unavailable data in js

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -11,10 +11,13 @@ import { AlertifyService } from '../_services/alertify.service';
 export class ShoppingCartComponent implements OnInit {
 
   cartData;
+  availableData;
   shoppingCartItemCount;
   totalPrice = 0;
   dataRetrieved = false;
-  constructor(private cartService: ShoppingCartService, private alertify: AlertifyService) {
+  availableCart: any = [];
+  unavailableCart: any;
+  constructor(private cartService: ShoppingCartService, private alertify: AlertifyService, private productService: ProductService) {
     console.log('count ' + this.shoppingCartItemCount);
     this.dataRetrieval();
    }
@@ -25,22 +28,75 @@ export class ShoppingCartComponent implements OnInit {
 
 
   dataRetrieval(){
-    this.cartService.getCart().subscribe(x => {
-      this.cartData = x;
-      this.dataRetrieved = true;
-      console.log(this.cartData);
-      this.getShoppingCartItemCount();
-      this.getTotalPrice();
-    }, error => {
-      this.dataRetrieved = true;
+    this.productService.getAll().subscribe(x => {
+      this.availableData = x;
+      this.cartService.getCart().subscribe(x => {
+        this.cartData = x;
+        this.dataRetrieved = true;
+        console.log(this.cartData);
+        console.log(this.availableData);
+        this.filterData();
+        this.getShoppingCartItemCount();
+        this.getTotalPrice();
+      }, error => {
+        this.dataRetrieved = true;
+      });
     });
+  }
+
+  filterData(){
+    const available = JSON.parse(JSON.stringify(this.cartData));
+    available.items = [];
+    const unavailable = JSON.parse(JSON.stringify(this.cartData));
+    unavailable.items = [];
+    console.log('--------------------------------------');
+  
+    console.log(available);
+    console.log(unavailable);
+    console.log('during filtering');
+    // tslint:disable-next-line: forin
+    for (const cartItemId in this.cartData.items){
+      console.log(this.cartData.items[cartItemId]);
+      let found = 0;
+      let product;
+      for (const productId in this.availableData){
+        if (this.cartData.items[cartItemId]._id == this.availableData[productId]._id){
+          found = 1;
+          product = this.availableData[productId];
+          break;
+        }
+      }
+      if (found == 1){
+        product.quantity = this.cartData.items[cartItemId].quantity;
+        available.items.push(product);
+        console.log('found');
+        
+      }else{
+        unavailable.items.push(this.cartData.items[cartItemId]);
+        console.log('not found');
+      }
+      // console.log(unavailable);
+      // console.log(available);
+    }
+    console.log('-----------------------------------------------------------------');
+    console.log('After filtering');
+    console.log(available);
+    console.log(unavailable);
+    this.availableCart = available;
+    if(unavailable.items.length == 0){
+      // this.unavailableCart = [];
+    }else{
+    this.unavailableCart = unavailable;
+    }
+    console.log(this.unavailableCart);
+  
   }
 
   getShoppingCartItemCount(){
     this.shoppingCartItemCount = 0;
     // tslint:disable-next-line: forin
-    for (const productId in this.cartData.items){
-      this.shoppingCartItemCount += this.cartData.items[productId].quantity;
+    for (const productId in this.availableCart.items){
+      this.shoppingCartItemCount += this.availableCart.items[productId].quantity;
     }
     console.log(this.shoppingCartItemCount);
   }
@@ -48,8 +104,8 @@ export class ShoppingCartComponent implements OnInit {
   getTotalPrice(){
     this.totalPrice = 0;
     // tslint:disable-next-line: forin
-    for (const productId in this.cartData.items) {
-      this.totalPrice += (this.cartData.items[productId].price * this.cartData.items[productId].quantity);
+    for (const productId in this.availableCart.items) {
+      this.totalPrice += (this.availableCart.items[productId].price * this.availableCart.items[productId].quantity);
     }
    // console.log(this.totalPrice);
   }
@@ -68,14 +124,15 @@ export class ShoppingCartComponent implements OnInit {
   }
 
   updateQuantity(cartData, change){
-    for (const product in this.cartData.items){
+    for (const product in this.availableCart.items){
       // tslint:disable-next-line: triple-equals
-      if (this.cartData.items[product]._id == cartData._id){
-        this.cartData.items[product].quantity += change;
+      if (this.availableCart.items[product]._id == cartData._id){
+        this.availableCart.items[product].quantity += change;
         // tslint:disable-next-line: triple-equals
-        if (this.cartData.items[product].quantity == 0) {
-          this.cartData.items.splice(product, 1);
+        if (this.availableCart.items[product].quantity == 0) {
+          this.availableCart.items.splice(product, 1);
         }
+        this.getShoppingCartItemCount();
         this.getTotalPrice();
         return;
       }
